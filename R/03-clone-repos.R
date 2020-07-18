3d9d3251c35a1d20da6b7f428807a4dc112e51e8
Matt Dray <mwdray@gmail.com>
2020-07-18 16:42:42 +0100

    Add protocol arg to allow for SSH, close #10

--- R/03-clone-repos.R
+++ R/03-clone-repos.R
@@ -4,21 +4,47 @@
 #'
 #' Clone a repo for a specified user to a specified local repository.
 #'
+#' If you're using \code{protocol = "ssh"}, you need to make sure you have
+#' \href{https://happygitwithr.com/ssh-keys.html}{set up your SSH keys}.
+#'
 #' @param gh_user Character string. A GitHub user name.
 #' @param repo Character string. A GitHub repo name for the named \code{gh_user}.
+#' @param protocol Character string. Either \code{"https"} or \code{"ssh"}.
 #' @param dest_dir Character string. A local file path where the zipped
 #'     repositories will be downloaded to. Must be a full path.
 #'
+#' @details If you're using \code{protocol = "ssh"}, you need to make sure
+#'     you've \href{https://happygitwithr.com/ssh-keys.html}{set up your SSH keys}.
+#'
 #' @return The named user's named repo cloned to the specified location.
-ghd_clone_one <- function(gh_user, repo, dest_dir) {
+ghd_clone_one <- function(gh_user, repo, protocol, dest_dir) {
 
-  # Pass a system call to clone the repo to the desination
-  system(
-    paste0(
-      "git clone https://github.com/", gh_user, "/", repo, ".git ",
-      dest_dir, "/", repo
+  if (!protocol %in% c("https", "ssh")) {
+    stop("You must provide either 'https' or 'ssh' to the protocol argument.")
+  }
+
+  # Pass a system call to clone the repo to the destination
+  # Different
+
+  if (protocol == "https") {
+
+    system(
+      paste0(
+        "git clone https://github.com/", gh_user, "/", repo, ".git ",
+        dest_dir, "/", repo
+      )
     )
-  )
+
+  } else if (protocol == "ssh") {
+
+    system(
+      paste0(
+        "git clone git@github.com:", gh_user, "/", repo, ".git ",
+        dest_dir, "/", repo
+      )
+    )
+
+  }
 
 }
 
@@ -29,11 +55,15 @@ ghd_clone_one <- function(gh_user, repo, dest_dir) {
 #'
 #' @param gh_user Character string. A GitHub user name.
 #' @param names_vec Character vector. Repo names for the given \code{gh_user}.
+#' @param protocol Character string. Either \code{"https"} or \code{"ssh"}.
 #' @param dest_dir Character string. A local file path where the zipped repositories
 #'     will be downloaded to. Must be a full path.
 #'
+#' @details If you're using \code{protocol = "ssh"}, you need to make sure
+#'     you've \href{https://happygitwithr.com/ssh-keys.html}{set up your SSH keys}.
+#'
 #' @return The named user's named repos cloned to the specified location.
-ghd_clone_multi <- function(gh_user, names_vec, dest_dir) {
+ghd_clone_multi <- function(gh_user, names_vec, protocol, dest_dir) {
 
   # Ask if all the repos should be downloaded
   q_clone_all <- readline(
@@ -51,6 +81,7 @@ ghd_clone_multi <- function(gh_user, names_vec, dest_dir) {
         ~ ghd_clone_one(
           gh_user = gh_user,
           repo = .x,
+          protocol = protocol,
           dest_dir = dest_dir
         )
       )
